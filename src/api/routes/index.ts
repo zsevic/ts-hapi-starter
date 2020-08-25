ccb1e072979fb670bd686a75e4ff0dd196776a82
zsevic <zsevic95@gmail.com>
2020-08-25 22:29:56 +0200

    Update Joi package

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,4 +1,5 @@
+import { ServerRoute } from '@hapi/hapi';
 import authorRoutes from './author';
 import postRoutes from './post';
 
-export default [...authorRoutes, ...postRoutes];
+export default [...authorRoutes, ...postRoutes] as ServerRoute[];
--- src/api/routes/post/validation.ts
+++ src/api/routes/post/validation.ts
@@ -1,4 +1,4 @@
-import Joi from '@hapi/joi';
+import Joi from 'joi';
 
 export const postQuery = Joi.object({
   name: Joi.string().optional(),
